5b026a90998e0f13d16eec887271000438618358
ELIZABETH WINNIE KURNIAWAN <i16005@ftis>
2018-11-02 13:57:29 +0700

    Initial commit

--- calculator.java
+++ calculator.java
@@ -1,4 +1,22 @@
 class Calculator
 {
-
+	public float plus (float a, float b)
+	{
+		
+	}
+	
+	public float minus (float a, float b)
+	{
+		
+	}
+	
+	public float multiply (float a, float b)
+	{
+		
+	}
+	
+	public float division (float a, float b)
+	{
+		
+	}
 }
