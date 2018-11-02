9b3e03148e5cedb57ceade0f3b8099c1fe0abafe
SAMUEL PANGIHUTAN SIHOMBING <i16054@ftis>
2018-11-02 14:13:57 +0700

    v2

--- calculator.java
+++ calculator.java
@@ -2,12 +2,12 @@ class Calculator
 {
 	public float plus (float a, float b)
 	{
-		
+		return a+b;
 	}
 	
 	public float minus (float a, float b)
 	{
-		
+		return a-b;
 	}
 	
 	public float multiply (float a, float b)
